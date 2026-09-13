221f532ddbb2e2a166f99e976422342671b900f4
Zaki <bushirizaki@gmail.com>
2026-09-13 12:54:59 -0400

    upd

--- exercises/04js/scripts/intro.js
+++ exercises/04js/scripts/intro.js
@@ -1,4 +1,16 @@
+// shows message when clicked
 document.getElementById("btn-show-message").onclick = (e) => {
     document.getElementById("p-message").innerHTML = "Hello World";
     e.target.innerHTML = "done!";
+};
+
+// styles the link when clicked
+document.getElementById("link").onclick = (e) => {
+    e.preventDefault(); //doesn't go to link destination
+    e.target.classList.add("cool-link");
+};
+
+// ball bounce once button clicked
+document.getElementById("btn-bounce").onclick = (e) => {
+    document.getElementById("ball").classList
 }
